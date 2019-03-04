6aaf0a4b23f3710260fdbbc1d355572a9119d721
krushnasoni <krushnasoni58@gmail.com>
2019-03-04 15:09:45 +0530

    second day

--- ecom/urls.py
+++ ecom/urls.py
@@ -6,4 +6,7 @@ app_name = 'ecom'
 urlpatterns = [
     path('sign_up', views.sign_up, name='sign_up'),
     path('sign_up_submit', views.sign_up_submit, name='sign_up_submit'),
+    path('login', views.login, name='login'),
+    path('login_submit', views.login_submit, name='login_submit'),
+    path('welcome', views.welcome, name='welcome'),
 ]
--- ecom/views.py
+++ ecom/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Users
 from ecom.forms import SignUpForm
-
+import hashlib
+from django.http import JsonResponse
 
 def sign_up(request):
     return render(request, 'ecom/sign_up.html')
@@ -14,10 +15,34 @@ def sign_up_submit(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             #form.save()
+            hashed_password = hashlib.sha512(request.POST['password'].encode('utf-8')).hexdigest()
+
             obj = form.save(commit=False)
+            obj.password = hashed_password
             obj.phone_number = "9835789000"
             obj.save()
             return render(request, 'ecom/login.html')
         else:
             return render(request, 'ecom/sign_up.html', {'form': form})
     return render(request, 'ecom/sign_up.html')
+
+def login(request):
+    return render(request, 'ecom/login.html')
+
+def login_submit(request):
+    hashed_password = hashlib.sha512(request.POST['password'].encode('utf-8')).hexdigest()
+    print(hashed_password)
+    #return HttpResponse(request.POST['password'])
+    try:
+        user = Users.objects.get(email=request.POST['email'])
+        print(user.password)
+        if user.password == hashed_password :
+            request.session['userdata'] = user.username
+            return JsonResponse({'status':'1'})
+        else:
+            return JsonResponse({'status':'2'})
+    except:
+        return JsonResponse({'status':'2'})
+
+def welcome(request):
+    return render(request, 'ecom/welcome.html',{'session_data':request.session['userdata']})
